5942d95bbd04392ef0d06d425b5b468a64f0b92f
YourGitHubUsername <your_email@example.com>
2025-02-06 21:04:12 +0530

    Added the the kafka Server with two spring projects for understanding

---
+++ endUser/src/main/java/com/endUser/EndUserApplication.java
@@ -0,0 +1,14 @@
+package com.endUser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+
+@SpringBootApplication
+public class EndUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndUserApplication.class, args);
+	}
+
+}
